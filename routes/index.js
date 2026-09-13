b70f763c8e535d8000b6ec55254c471f7f40f821
RuBublik <relikr@gmail.com>
2026-09-13 19:31:57 +0300

    Add schemas page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const schemas = require('../data/schemas');
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// schemas page: the data is rendered into the HTML on the server (no fetch in the browser)
+router.get('/schemas', (req, res) => {
+  res.render('schemas', { resources: schemas });
 });
 
 module.exports = router;
